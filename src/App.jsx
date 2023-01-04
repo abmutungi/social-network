61e85d19130485aae7177284f8889d70e00ac990
Arnold Mutungi <abmutungi@gmail.com>
2023-01-04 14:58:33 +0000

    starting routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
 import ReactDOM from "react-dom/client";
-// import ProfileBar  from "./Profile/ProfileBar";
-// import AllChats from "./AllChats/AllChats";
 import TopHeader from "./tb38r/TopHeader";
-import {  LowerHeader, MainBody } from "./tb38r/Leftside";
-import "./index.css"
-
-
+import { LowerHeader, MainBody } from "./tb38r/Leftside";
 // import {ChatBox} from "./ChatBox/ChatBoxComponent"
-// import {Login} from "./Login/LoginComponent"
-// import {Register} from "./Register/RegistrationComponent"
+import {Login} from "./Login/LoginComponent"
+import {Register} from "./Register/RegistrationComponent"
+import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 
 
 
 const App = () => {
   return (
     <>
-      <TopHeader />
-      <LowerHeader />
-      <MainBody />
+    <nav>
+      <ul>
+        <li> 
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/register" element={<Register/>}>
+      </Route>
+      <Route path="/login" element={<Login/>}>
+      </Route>
+        <Route path="/" element={(
+          <>
+            <TopHeader />
+            <LowerHeader />
+            <MainBody />
+          </>
+        )}> 
+        </Route>
+
+       {/* <ChatBox/> */}
+    </Routes>
     </>
   );
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+</BrowserRouter>
+);
