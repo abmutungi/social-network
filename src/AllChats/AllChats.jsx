68f8ac8506e2ce1e56eb3e4c68e980ab75b88990
tb38r <tlawal86@hotmail.co.uk>
2023-01-10 14:28:16 +0000

    refactored AllChats, todo others

--- src/AllChats/AllChats.jsx
+++ src/AllChats/AllChats.jsx
@@ -2,30 +2,43 @@ import React from "react";
 import ChatProfile from "./ChatProfile";
 import "./AllChats.css";
 
+const TestUsers = ["Tolu", "Sarmad", "Arnold", "Yonas"];
+
+/*Key arg in map required, currently using arr index, 
+could/should use userID instead */
+const ChatUsers = ({ users }) => {
+
+  return users.map((user, index) => {
+    return <ChatProfile chatName={`${user}`} key={index} />;
+  });
+};
+
+
+const UserChats = ({ Users }) => {
+  return (
+    <div className="UserChats">
+      <div className="ChatTitle">Chats</div>
+      <ChatUsers users={Users} />
+    </div>
+  );
+};
+
+
+
 const AllChats = () => {
-    return (
-        <div className="AllChats">
-            <div className="UserChats">
-                <div className="ChatTitle">Chats</div>
-                <ChatProfile chatName={"Arnold Mutungi"} />
-                <ChatProfile chatName={"Sarmad Khatri"} />
-                <ChatProfile chatName={"Tolu Lawal"} />
-                <ChatProfile chatName={"Yonas Million"} />
-            </div>
-            {/* <div className="ChatTitle">Group Chats</div>
-            <ChatProfile chatName={"test1"} />
-            <ChatProfile chatName={"test2"} />
-            <ChatProfile chatName={"test3"} />
-            <ChatProfile chatName={"test4"} /> */}
-                 <div className="UserChats">
-                <div className="ChatTitle">Group Chats</div>
-                <ChatProfile chatName={"test1"} />
-            <ChatProfile chatName={"test2"} />
-            <ChatProfile chatName={"test3"} />
-            <ChatProfile chatName={"test4"} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="AllChats">
+      <UserChats Users={TestUsers} />
+
+      <div className="UserChats">
+        <div className="ChatTitle">Group Chats</div>
+        <ChatProfile chatName={"test1"} />
+        <ChatProfile chatName={"test2"} />
+        <ChatProfile chatName={"test3"} />
+        <ChatProfile chatName={"test4"} />
+      </div>
+    </div>
+  );
 };
 
 export default AllChats;
