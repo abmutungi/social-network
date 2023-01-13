ba3f28dbe81a7c7720c49fcd802f001b785a221c
nsym-coding <nsym.coding@outlook.com>
2023-01-13 11:03:18 +0000

    got modal working for createGroup in terms of appearance and behaviour, need to now do logic for creating group

--- src/Groups/Groups.jsx
+++ src/Groups/Groups.jsx
@@ -1,6 +1,7 @@
 import ChatProfile from "../AllChats/ChatProfile";
 import "./Groups.css";
 import { ProfileBtn } from "../Profile/ProfileBtn";
+import { useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 // import { faShareFromSquare } from "@fortawesome/free-regular-svg-icons";
 import {
@@ -9,9 +10,62 @@ import {
   faCirclePlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { CreateGroupModal } from "./CreateGroupComponent";
 library.add(faCirclePlus, faLock, faUsers);
 
+import {
+  
+  
+  faXmark,
+ 
+} from "@fortawesome/free-solid-svg-icons";
+
+const CreateGroupModal = ({show, onClose}) => {
+
+    if (!show) {
+        return null
+    }
+
+    return(
+        <>
+
+        <div className="cg-modal-container" onClick={onClose}>
+        <div className="cg-modal" onClick={e => e.stopPropagation()}>
+            <div className="cg-modal-header">
+                <h3>Create Group</h3>     
+          <FontAwesomeIcon
+          onClick={ onClose}
+            icon={faXmark}
+            className="cg-modal-close"
+            size="lg"
+          />
+            </div>
+            <div className="cg-modal-body">
+                <form className="cg-form">
+                    <input className="cg-input" type="text" placeholder="enter group name"></input>
+                    <input className="cg-input" type="text" placeholder="enter group description"></input>
+                         <label htmlFor="avatar">
+            Choose an image to be your group avatar (Optional)
+          </label>
+          <input
+            type="file"
+            name="group-avatar"
+           
+          />
+                </form>
+            </div>
+            <div className="cg-modal-footer">
+                    <button className="cg-submit-button" type="submit">Submit</button>
+                    </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
 const NamedGroup = ({ groups, groupclass}) => {
+
+  
   
     return groups.map((user, index) => {
       return <ChatProfile chatName={`${user}`} key={index} chatuserclass = {groupclass} />;
@@ -20,9 +74,11 @@ const NamedGroup = ({ groups, groupclass}) => {
   
   
   const Groups = ({ Users }) => {
+    const [show, setShow] = useState(false)
     return (
       <div className={Users.GroupClasses.parent}>
-        <div className="ChatTitle">{Users.Headers.Groups}   <FontAwesomeIcon className="create-group-btn"  icon="fa-solid fa-circle-plus" />
+        <div className="ChatTitle">{Users.Headers.Groups}  <FontAwesomeIcon   onClick={() => setShow(true)} className="cg-btn"  icon="fa-solid fa-circle-plus" />
+        <CreateGroupModal onClose={() => setShow(false)} show={show}/>
         </div>
         <div className="AllCumulativeData">
 
