24fff2ef6d5a6eb99b1aaf6600348fc33a0c8443
eternal17 <sarmadkhatri15@gmail.com>
2023-01-15 14:21:25 +0000

    added props to the notification component

--- src/Notifications/notificationsModal.jsx
+++ src/Notifications/notificationsModal.jsx
@@ -1,5 +1,29 @@
 import "./notifications-modal.css";
 
+// single notifications component
+const SingleNotificationComponent = ({ props }) => {
+  return (
+    <div className="notification-container">
+      <div className="notifs-profile-content">
+        <img
+          className="notifs-profile cp-profile-pic"
+          src={props.profileImgPath}
+          alt="img"
+        />
+        <div className="notifs-content-date">
+          <div className="notifs-content">
+            <strong>{props.name}</strong> sent you a {props.notificationType}
+          </div>
+          <div className="notifs-date">01/01</div>
+        </div>
+      </div>
+      <div className="notifs-action">
+        <button className="confirm-button">Confirm</button>
+        <button>Remove</button>
+      </div>
+    </div>
+  );
+};
 // notifications modal that holds single notifications
 const NotificationsModal = ({ show, onClose }) => {
   if (!show) {
@@ -16,44 +40,38 @@ const NotificationsModal = ({ show, onClose }) => {
         <div className="notifs-modal-header"></div>
         <div className="notifs-modal-title">Notifications</div>
         <div className="notifs-modal-body">
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
-          <SingleNotificationComponent />
+          <SingleNotificationComponent
+            props={{
+              profileImgPath: "./Posts/man-utd.png",
+              name: "Cristiano Ronaldo",
+              notificationType: "follow request",
+            }}
+          />
+          <SingleNotificationComponent
+            props={{
+              profileImgPath: "./Posts/man-utd.png",
+              name: "Cristiano Ronaldo",
+              notificationType: "follow request",
+            }}
+          />
+          <SingleNotificationComponent
+            props={{
+              profileImgPath: "./Posts/man-utd.png",
+              name: "Cristiano Ronaldo",
+              notificationType: "follow request",
+            }}
+          />
+          <SingleNotificationComponent
+            props={{
+              profileImgPath: "./Posts/man-utd.png",
+              name: "Cristiano Ronaldo",
+              notificationType: "follow request",
+            }}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-// single notifications component
-const SingleNotificationComponent = () => {
-  return (
-    <div className="notification-container">
-      <div className="notifs-profile-content">
-        <img
-          className="notifs-profile cp-profile-pic"
-          src="./Posts/man-utd.png"
-          alt="img"
-        />
-        <div className="notifs-content-date">
-          <div className="notifs-content">
-            Cristiano Ronaldo sent you a friend request
-          </div>
-          <div className="notifs-date">a week ago</div>
-        </div>
-      </div>
-      <div className="notifs-action">
-        <button className="confirm-button">Confirm</button>
-        <button>Remove</button>
-      </div>
-    </div>
-  );
-};
 export { NotificationsModal };
