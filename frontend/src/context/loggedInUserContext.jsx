d51edb484804e761addc7b4da3b0a966389d8c95
nsym-coding <nsym.coding@outlook.com>
2023-02-02 18:26:35 +0000

    got refresh working in terms of storing loggedInUser in local storage, need to work on storing the other context in local storage as well

--- frontend/src/context/loggedInUserContext.jsx
+++ frontend/src/context/loggedInUserContext.jsx
@@ -3,9 +3,13 @@ import { createContext, useState } from "react";
 const loggedInUserContext = createContext({});
 
 function LoggedInUserProvider({ children }) {
-  const [loggedInUser, setLoggedInUser] = useState({});
+  const [loggedInUser, setLoggedInUser] = useState(() => {
+    const storedUser = localStorage.getItem("loggedInUser");
+    return storedUser ? JSON.parse(storedUser) : {};
+  });
   function updateLoggedInUser(user) {
     setLoggedInUser(user);
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
   }
 
   return (
