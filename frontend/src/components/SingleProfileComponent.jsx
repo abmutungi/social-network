2f98929e3760fb6989a7695180738b54814f7c91
Arnold Mutungi <abmutungi@gmail.com>
2023-03-28 13:16:40 +0100

    test single profile component - db lock bug

--- frontend/src/components/SingleProfileComponent.jsx
+++ frontend/src/components/SingleProfileComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../assets/css/AllChats.css";
 import "../assets/css/Users.css";
 import { LowerHeaderContext } from "../context/lowerheadercontext";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { followText, unfollowText, requestText } from "./UserRequestBtn";
 import { useNavigate } from "react-router-dom";
 import { ChatBox } from "../pages/home/components/ChatBoxComponent";
@@ -35,74 +35,77 @@ const SingleProfileComponent = (props) => {
 
   const [groupClicked, setGroupClicked] = useState(false);
   const navigate = useNavigate();
-  // const handleClick = () => {
-  async function FetchRelationship() {
-    try {
-      const response = await fetch("http://localhost:8080/followCheck", {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({
-          loggedInUserID: LoggedInUserID,
-          userID: userID,
-        }),
-      });
-      const data = await response.json();
+   const handleClick = async () => {
+    // async function FetchRelationship() {
+      try {
+        const response = await fetch("http://localhost:8080/followCheck", {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify({
+            loggedInUserID: LoggedInUserID,
+            userID: userID,
+          }),
+        });
+        const data = await response.json();
 
-      if (data.canFollow) {
-        updateFollowing(false);
-        updateFollowText(followText);
-      } else if (data.following) {
-        updateFollowing(true);
-        updateFollowText(unfollowText);
-      } else if (data.requested) {
-        updateRequested(true);
-        updateFollowText(requestText);
+        if (data.canFollow) {
+          updateFollowing(false);
+          updateFollowText(followText);
+        } else if (data.following) {
+          updateFollowing(true);
+          updateFollowText(unfollowText);
+        } else if (data.requested) {
+          updateRequested(true);
+          updateFollowText(requestText);
+        } else {
+          updateFollowText(followText);
+        }
+
+        if (data.msg) {
+          navigate("/login");
+          return;
+        }
+      } catch (e) {
+        console.log("error fetching relationshiip", e);
       }
+    
+  }
+  const handleGroupClick = async () => {
+    // async function FetchGroupInfo() {
+      //updateGroupRequested(false)
+      try {
+        const response = await fetch("http://localhost:8080/isgroupmember", {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify({
+            loggedInUserID: LoggedInUserID,
+            groupID: GroupID,
+          }),
+        });
+        const data = await response.json();
+        updateisGroupMember(data.ismember);
+        updateGroupRequested(data.requested);
 
-      if (data.msg) {
-        navigate("/login");
-        return;
+        //console.log("response from fetchgroupinfo", data);
+
+        // console.log("response from fetchgroupinfo", data);
+      } catch (e) {
+        console.log("error fetching groupinfo", e);
       }
-    } catch (e) {
-      console.log("error fetching relationshiip", e);
     }
-  }
-
-  async function FetchGroupInfo() {
-    //updateGroupRequested(false)
-    try {
-      const response = await fetch("http://localhost:8080/isgroupmember", {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({
-          loggedInUserID: LoggedInUserID,
-          groupID: GroupID,
-        }),
-      });
-      const data = await response.json();
-      updateisGroupMember(data.ismember);
-      updateGroupRequested(data.requested);
-
-      //console.log("response from fetchgroupinfo", data);
-
-      // console.log("response from fetchgroupinfo", data);
-    } catch (e) {
-      console.log("error fetching groupinfo", e);
-    }
-  }
-
-  useEffect(() => {
-    if (userID > 0) FetchRelationship();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userID]);
+  
+  // useEffect(() => {
+  //   if (userID > 0) FetchRelationship();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [userID]);
 
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, [userID]);
 
-  useEffect(() => {
-    if (GroupID > 0) FetchGroupInfo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [GroupID]);
+  // useEffect(() => {
+  //   if (GroupID > 0) FetchGroupInfo();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [GroupID]);
 
   // fetch chat history on chat user click
   const fetchChatsForm = new FormData();
@@ -159,6 +162,7 @@ const SingleProfileComponent = (props) => {
           updateNavClicked(false);
           updateUserID(Number(e.currentTarget.id));
           updateDynamicID(e.currentTarget.id);
+          handleClick()
         }}
         className="SingleProfile"
         id={props.id}
@@ -182,6 +186,8 @@ const SingleProfileComponent = (props) => {
           e.preventDefault();
           updateNavClicked(false);
           updateGroupID(Number(e.currentTarget.id));
+          handleGroupClick()
+
         }}
         className="SingleProfile"
         id={props.id}
