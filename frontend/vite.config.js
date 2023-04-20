97e33c48bb973c8027f8be6633e2858b8d542503
Tolu <tlawal86@hotmail.co.uk>
2023-04-20 15:51:12 +0100

    runs from docker but with broken images

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,22 @@ export default defineConfig({
   server:{
     watch:{
       usePolling: true
-    }
-  }
-});
+    },
+    host: '0.0.0.0',
+    port: 5173,
+  }, 
+  preview:{
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    },
+    chunkSizeWarningLimit: 600,
+}});
